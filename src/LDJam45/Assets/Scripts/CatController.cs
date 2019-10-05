fb21caa55ffd11e0d7b90492fa82924cadeaa847
Mark Arneman <mark@arneman.me>
2019-10-04 21:12:08 -0400

    Button Based Input, Dashing, Jumping

--- src/LDJam45/Assets/Scripts/CatController.cs
+++ src/LDJam45/Assets/Scripts/CatController.cs
@@ -4,44 +4,58 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-    [SerializeField] private float Speed = 3.0f;    
-    [SerializeField] private float MaxSpeed = 5.0f;
+    [SerializeField] private float Speed = 3.0f;
+    [SerializeField] private float DashForce = 1.5f;
+    [SerializeField] private float JumpForce = 5.0f;
+
+    private bool Jumping = false;
+    private bool Dashing = false;
 
     private float TransitionSpeed;
     private float MovementSpeed;
     private Rigidbody rb;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
         MovementSpeed = Speed;
-        TransitionSpeed = 0;
+        TransitionSpeed = 0;        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {   
+    void FixedUpdate() {   
         float MoveVertical = Input.GetAxis("Vertical");
         float MoveHorizontal = Input.GetAxis("Horizontal");        
-        Vector3 Movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical).normalized;        
+        Vector3 Movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical).normalized;
 
-        // Sprinting 
-        if (Input.GetKey(KeyCode.LeftShift)) {            
-            if (TransitionSpeed < 1) {
-                TransitionSpeed += 1.0f * Time.deltaTime;
-                MovementSpeed = Mathf.Lerp(Speed, MaxSpeed, TransitionSpeed);                
-            }            
-        } else {
-            if (TransitionSpeed >= 1.0f) {
-                TransitionSpeed = 0;
-            }
-            if (TransitionSpeed < 1 && MovementSpeed > Speed) {               
-                TransitionSpeed += 1.0f * Time.deltaTime;
-                MovementSpeed = Mathf.Lerp(MovementSpeed, Speed, TransitionSpeed);                
+        // Dashing
+        if (!Jumping && !Dashing) {            
+            if (Input.GetButton("Dash")) {
+                Dashing = true;
+                rb.AddForce(Movement * DashForce, ForceMode.Impulse);
+                StartCoroutine(CatDash());
             }
         }
 
+        if (!Jumping && !Dashing) {            
+            if (Input.GetButton("Jump")) {
+                Jumping = true;
+                Movement.y += JumpForce;
+                rb.AddForce(Movement, ForceMode.Impulse);                                
+                StartCoroutine(CatJump());
+            }
+        }        
+
         rb.MovePosition(transform.position + Movement * MovementSpeed * Time.deltaTime);                 
+    }    
+
+    IEnumerator CatJump() {        
+        yield return new WaitForSeconds(1.0f);
+        Jumping = false;
+    }
+
+    IEnumerator CatDash() {
+        yield return new WaitForSeconds(1.0f);
+        Dashing = false;
     }
 }
