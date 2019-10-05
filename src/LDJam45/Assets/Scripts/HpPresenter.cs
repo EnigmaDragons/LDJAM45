fca3b56dd2cbbe1c301e3dbfc2d59eac845a9b06
SilasReinagel <silas.reinagel@gmail.com>
2019-10-04 20:26:23 -0700

    Create HP Display. Closes #40

---
+++ src/LDJam45/Assets/Scripts/HpPresenter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class HpPresenter : MonoBehaviour
+{
+    [SerializeField] private GameEvent onHealthLost;
+    [SerializeField] private GameEvent onHealthGained;
+    [SerializeField, ReadOnly] private int currentHealth;
+
+    private GameObject[] hpIcons;
+
+    private void OnEnable()
+    {
+        onHealthGained.Subscribe(IncrementHealth, this);
+        onHealthLost.Subscribe(DecrementHealth, this);
+        hpIcons = new GameObject[transform.childCount];
+        for (int i = 0; i < hpIcons.Length; ++i)
+            hpIcons[i] = transform.GetChild(i).gameObject;
+    }
+
+    private void OnDisable()
+    {
+        onHealthLost.Unsubscribe(this);
+        onHealthGained.Unsubscribe(this);
+    }
+
+    void DecrementHealth() => UpdateHealth(-1);
+    void IncrementHealth() => UpdateHealth(1);
+
+    void UpdateHealth(int amount)
+    {
+        currentHealth += amount;
+        for (int i = 0; i < hpIcons.Length; ++i)
+            hpIcons[i].SetActive(i < currentHealth);
+    }
+}
