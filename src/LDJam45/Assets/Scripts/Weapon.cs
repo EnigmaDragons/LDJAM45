1ddad7d4671d180d9087e3beebfccd5f49403b23
noah reinagel <noah.r9@gmail.com>
2019-10-04 18:38:10 -0700

    work towards laser turret AI

---
+++ src/LDJam45/Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponName", menuName = "Weapon", order = 1)]
+class Weapon : ScriptableObject
+{
+    [SerializeField] public float FireInterval = 0.6f;
+    [SerializeField] public int NumProjectiles = 1;
+    [SerializeField] public GameObject ProjectilePrototype;
+    [SerializeField] public int Damage;
+    [SerializeField] public float DelayBetweenShots = 0.18f;
+}
---
+++ src/LDJam45/Assets/Scripts/WeaponBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    [SerializeField] private Weapon Weapon;
+    [SerializeField] private Vector3 ProjectileOffset;
+    [SerializeField] private Role Role;
+
+    private float _msBeforeFire = 0;
+
+    private void Update()
+    {
+        if (_msBeforeFire > 0)
+            _msBeforeFire -= Time.deltaTime;
+    }
+
+    public void FireTowards(Vector3 target)
+    {
+        var targetDir = target - transform.position;
+        var newDir = Vector3.RotateTowards(transform.forward, targetDir, float.MaxValue, 0.0f);
+        var targetRotation = Quaternion.LookRotation(newDir);
+
+        Fire(targetRotation);
+    }
+
+    public void Fire()
+    {
+        Fire(transform.rotation);
+    }
+
+    private void Fire(Quaternion rotation)
+    {
+        if (_msBeforeFire > 0)
+            return;
+
+        _msBeforeFire = Weapon.FireInterval;
+        StartCoroutine(LaunchProjectiles(rotation));
+    }
+
+    private IEnumerator LaunchProjectiles(Quaternion rotation)
+    {
+        for (var i = 0; i < Weapon.NumProjectiles; i++)
+        {
+            var spawnPos = transform.position + ProjectileOffset;
+            var p = Instantiate(Weapon.ProjectilePrototype, spawnPos, rotation);
+            var projectile = p.GetComponent<ParticleCollisionInstance>();
+            projectile.Damage = Weapon.Damage;
+            projectile.OwnedBy = Role;
+            yield return new WaitForSeconds(Weapon.DelayBetweenShots);
+        }
+    }
+}
