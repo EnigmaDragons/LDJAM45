b6f8a13666c2e87391800ddf1f62d47a7834719a
SilasReinagel <silas.reinagel@gmail.com>
2019-10-05 02:38:26 -0700

    Can move camera between screens. Closes #57

---
+++ src/LDJam45/Assets/Scripts/MoveCameraOnEnter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MoveCameraOnEnter : MonoBehaviour
+{
+    [SerializeField] private Camera sceneCamera;
+    [SerializeField] private GameObject newPosition;
+    [SerializeField] private float speed = 90f;
+
+    private bool _isStarted;
+    private bool _isFinished;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isStarted = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isStarted = true;
+    }
+
+    private void Update()
+    {
+        if (!_isStarted || _isFinished)
+            return;
+
+        if (sceneCamera.transform.position.Equals(newPosition.transform.position))
+        {
+            _isFinished = true;
+            return;
+        }
+
+        sceneCamera.transform.position = Vector3.MoveTowards(sceneCamera.transform.position, newPosition.transform.position, speed * Time.deltaTime);
+    }
+}
