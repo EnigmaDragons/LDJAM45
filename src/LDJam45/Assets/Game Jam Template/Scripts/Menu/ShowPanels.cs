801c3826667b3781cf910c5786d3839b0491917d
Jaroslav Vozár <jarkovozar@gmail.com>
2019-10-05 21:52:21 +0200

    Imported and implemented Main menu buttons art Improved menu selection using keyboard (arrow keys) and gamepad

--- src/LDJam45/Assets/Game Jam Template/Scripts/Menu/ShowPanels.cs
+++ src/LDJam45/Assets/Game Jam Template/Scripts/Menu/ShowPanels.cs
@@ -19,19 +19,19 @@ public class ShowPanels : MonoBehaviour {
 
     private void SetSelection(GameObject panelToSetSelected)
     {
-        activePanel = panelToSetSelected;
+        //activePanel = panelToSetSelected;
         activePanelMenuObject = activePanel.GetComponent<MenuObject>();
 
         if (activePanelMenuObject != null)
         {
             activePanelMenuObject.SetFirstSelected();
         }
+
+        menuSelected = true;
     }
 
     public void Start()
-    {
-        SetSelection(menuPanel);
-
+    {       
         HideCreditsPanel();
         HideOptionsPanel();
         HidePausePanel();
@@ -43,8 +43,10 @@ public class ShowPanels : MonoBehaviour {
 		optionsPanel.SetActive(true);
 		optionsTint.SetActive(true);
         menuPanel.SetActive(false);
-        SetSelection(optionsPanel);
-    }
+
+	    activePanel = optionsPanel;
+	    //SetSelection(optionsPanel);
+	}
 
 	//Call this function to deactivate and hide the Options panel during the main menu
 	public void HideOptionsPanel()
@@ -52,6 +54,8 @@ public class ShowPanels : MonoBehaviour {
         menuPanel.SetActive(true);
         optionsPanel.SetActive(false);
 		optionsTint.SetActive(false);
+
+	    activePanel = menuPanel;
 	}
 
     //Call this function to activate and display the Options panel during the main menu
@@ -60,7 +64,9 @@ public class ShowPanels : MonoBehaviour {
         creditsPanel.SetActive(true);
         creditsTint.SetActive(true);
         menuPanel.SetActive(false);
-        SetSelection(creditsPanel);
+
+        activePanel = creditsPanel;
+        //SetSelection(creditsPanel);
     }
 
     //Call this function to deactivate and hide the Options panel during the main menu
@@ -69,33 +75,55 @@ public class ShowPanels : MonoBehaviour {
         menuPanel.SetActive(true);
         creditsPanel.SetActive(false);
         creditsTint.SetActive(false);
+
+        activePanel = menuPanel;
     }
 
     //Call this function to activate and display the main menu panel during the main menu
     public void ShowMenu()
 	{
-		menuPanel.SetActive (true);
-        SetSelection(menuPanel);
+		menuPanel.SetActive(true);
+
+	    activePanel = menuPanel;
+        //SetSelection(menuPanel);
     }
 
 	//Call this function to deactivate and hide the main menu panel during the main menu
 	public void HideMenu()
 	{
-		menuPanel.SetActive (false);
+		menuPanel.SetActive(false);
 	}
 	
 	//Call this function to activate and display the Pause panel during game play
 	public void ShowPausePanel()
 	{
-		pausePanel.SetActive (true);
+		pausePanel.SetActive(true);
 		optionsTint.SetActive(true);
-        SetSelection(pausePanel);
+
+	    activePanel = pausePanel;
+        //SetSelection(pausePanel);
     }
 
 	//Call this function to deactivate and hide the Pause panel during game play
 	public void HidePausePanel()
 	{
-		pausePanel.SetActive (false);
+		pausePanel.SetActive(false);
 		optionsTint.SetActive(false);
 	}
+
+    private bool menuSelected = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            menuSelected = false;
+            return; //ignore mouse click
+        }        
+
+        if (Input.anyKeyDown && !menuSelected)
+        {
+            SetSelection(activePanel);
+        }
+    }
 }
