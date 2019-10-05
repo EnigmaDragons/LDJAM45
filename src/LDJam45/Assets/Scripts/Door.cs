aca1e54e9ca8ec0c21808aa6981b8250034a4eb2
SilasReinagel <silas.reinagel@gmail.com>
2019-10-04 22:24:52 -0700

    Door can open and close. Closes #38

---
+++ src/LDJam45/Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField, ReadOnly] private bool isOpen = false;
+    [SerializeField] private GameEvent openDoorTrigger;
+    [SerializeField] private GameObject openDoor;
+    [SerializeField] private GameObject closedDoor;
+    [SerializeField] private bool startsOpen = false;
+
+    private void Awake()
+    {
+        isOpen = startsOpen;
+        UpdateDoorState();
+    }
+
+    private void OnEnable()
+    {
+        openDoorTrigger?.Subscribe(OpenDoor, this);
+    }
+
+    private void OnDisable()
+    {
+        openDoorTrigger?.Unsubscribe(this);
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+        UpdateDoorState();
+    }
+
+    void UpdateDoorState()
+    {
+        openDoor.SetActive(isOpen);
+        closedDoor.SetActive(!isOpen);
+    }
+}
