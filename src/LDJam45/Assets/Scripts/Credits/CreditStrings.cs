147aedbb5b24aab3befee5c468839f7cf4549a3b
noah reinagel <noah.r9@gmail.com>
2019-10-06 15:34:06 -0700

    began build credits screen

---
+++ src/LDJam45/Assets/Scripts/Credits/CreditStrings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditStrings : MonoBehaviour
+{
+    [SerializeField] private List<string> Strings;
+    [SerializeField] private Text Text;
+    private int _index;
+
+    private void Start()
+    {
+        _index = 0;
+        Text.text = Strings[_index];
+    }
+
+    public void Next()
+    {
+        _index++;
+        Text.text = _index >= Strings.Count ? "" : Strings[_index];
+    }
+}
