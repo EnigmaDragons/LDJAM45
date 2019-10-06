2df5be61574e0bf7e5b60c1ecddff49d5cb365e9
SilasReinagel <silas.reinagel@gmail.com>
2019-10-05 22:55:29 -0700

    Start music in Game. Closes #97

--- src/LDJam45/Assets/Game Jam Template/Scripts/Menu/PlayMusic.cs
+++ src/LDJam45/Assets/Game Jam Template/Scripts/Menu/PlayMusic.cs
@@ -1,19 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PlayMusic : MonoBehaviour {
 
-
+    public GameMusicPlayer musicPlayer;
     public MenuSettings menuSettings;
-	public AudioClip titleMusic;					//Assign Audioclip for title music loop
-	public AudioClip mainMusic;						//Assign Audioclip for main 
 	public AudioMixerSnapshot volumeDown;			//Reference to Audio mixer snapshot in which the master volume of main mixer is turned down
 	public AudioMixerSnapshot volumeUp;				//Reference to Audio mixer snapshot in which the master volume of main mixer is turned up
 
-
-	private AudioSource musicSource;				//Reference to the AudioSource which plays music
+    private AudioSource musicSource;				//Reference to the AudioSource which plays music
 	private float resetTime = .01f;					//Very short time used to fade in near instantly without a click
 
 
@@ -21,9 +17,9 @@ public class PlayMusic : MonoBehaviour {
 	{
 		//Get a component reference to the AudioSource attached to the UI game object
 		musicSource = GetComponent<AudioSource> ();
-		//Call the PlayLevelMusic function to start playing music
+        musicPlayer.MusicSource = musicSource;
+        PlayLevelMusic();
 	}
-
 
 	public void PlayLevelMusic()
 	{
@@ -39,14 +35,12 @@ public class PlayMusic : MonoBehaviour {
                 Debug.Log("Scene index is 1, setting music to " + menuSettings.musicLoopToChangeTo);
 				musicSource.clip = menuSettings.musicLoopToChangeTo;
 				break;
-
 		}
 
-
 		//Fade up the volume very quickly, over resetTime seconds (.01 by default)
-		FadeUp (resetTime);
+		FadeUp(resetTime);
 		//Play the assigned music clip in musicSource
-		musicSource.Play ();
+		musicSource.Play();
 	}
 	
 	//Used if running the game in a single scene, takes an integer music source allowing you to choose a clip by number and play.
