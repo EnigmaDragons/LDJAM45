398ddb849c375809587920fff671f956f61c2053
SilasReinagel <silas.reinagel@gmail.com>
2019-10-05 23:32:57 -0700

    Created script to enable custom cursor. Closes #107

---
+++ src/LDJam45/Assets/Scripts/UseCustomCursor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UseCustomCursor : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursor;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
