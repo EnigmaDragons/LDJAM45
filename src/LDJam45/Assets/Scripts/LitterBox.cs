8fdf252ab7f437a802bd09c75fac46b9b73abad5
Mark Arneman <mark@arneman.me>
2019-10-06 08:12:11 -0400

    Litterbox in switch room

---
+++ src/LDJam45/Assets/Scripts/LitterBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitterBox : MonoBehaviour
+{
+    [SerializeField] GameObject DirtyLitterBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (!DirtyLitterBox.activeSelf) {
+                DirtyLitterBox.SetActive(true);
+            }
+        }
+        
+        Debug.Log(other.gameObject.tag.ToString());
+    }
+}
