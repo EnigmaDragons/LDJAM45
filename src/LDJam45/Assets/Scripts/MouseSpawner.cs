67cd8ae9e6d5fc54480606d3284ab93835a0e89d
noah reinagel <noah.r9@gmail.com>
2019-10-06 16:04:51 -0700

    roomba deals damage again mouse room works

--- src/LDJam45/Assets/Scripts/MouseSpawner.cs
+++ src/LDJam45/Assets/Scripts/MouseSpawner.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class MouseSpawner : MonoBehaviour
 {
-
+    [SerializeField] private GameEvent MouseRoomStarted;
     [SerializeField] GameObject MousePrefab;
     [SerializeField] float SpawnTimer = 1.0f;
     [SerializeField] float SpawnDuration = 15.0f;
 
+    private bool StartSpawing = false;
     private bool StopSpawning = false;
     private bool HasSpawned = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnCountdown());
+        MouseRoomStarted.Subscribe(() => StartCoroutine(SpawnCountdown()), this);
     }
+
+    void OnDisable() => MouseRoomStarted.Unsubscribe(this);
 
     // Update is called once per frame
     void Update()
     {
-        if (!StopSpawning && !HasSpawned) {
+        if (StartSpawing && !StopSpawning && !HasSpawned)
+        {
             StartCoroutine(SpawnMouse());
         }
     }
 
-    IEnumerator SpawnMouse() {
+    IEnumerator SpawnMouse()
+    {
         HasSpawned = true;
         Instantiate(MousePrefab, this.transform);
         yield return new WaitForSeconds(SpawnTimer);
         HasSpawned = false;
     }
 
-    IEnumerator SpawnCountdown() {
+    IEnumerator SpawnCountdown()
+    {
+        StartSpawing = true;
         yield return new WaitForSeconds(SpawnDuration);
         StopSpawning = true;
     }
