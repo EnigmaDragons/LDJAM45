3771302d0c06a3ad4f6cb716cf8c0a68231720bb
Jaroslav Vozár <jarkovozar@gmail.com>
2019-10-06 13:49:10 +0200

    Added click state for custom cursor

--- src/LDJam45/Assets/Scripts/UseCustomCursor.cs
+++ src/LDJam45/Assets/Scripts/UseCustomCursor.cs
@@ -2,10 +2,29 @@
 
 public class UseCustomCursor : MonoBehaviour
 {
-    [SerializeField] private Texture2D cursor;
+    [SerializeField]
+    private Texture2D cursorIdle;
+
+    [SerializeField]
+    private Texture2D cursorClick;
+
+    private readonly Vector2 hotSpot = new Vector2(4, 5);
 
     private void Awake()
     {
-        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(cursorIdle, hotSpot, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(cursorClick, hotSpot, CursorMode.Auto);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(cursorIdle, hotSpot, CursorMode.Auto);
+        }
     }
 }
