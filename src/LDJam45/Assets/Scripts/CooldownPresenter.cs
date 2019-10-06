f545522441fb11e54410ab7e4f2b72988d0f0326
SilasReinagel <silas.reinagel@gmail.com>
2019-10-05 21:02:36 -0700

    Doesn't show cooldown until ability is unlocked. Closes #109

---
+++ src/LDJam45/Assets/Scripts/CooldownPresenter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CooldownPresenter : MonoBehaviour
+{
+    [SerializeField] private GameObject dashHud;
+    [SerializeField] private GameObject laserHud;
+    [SerializeField] private GameState state;
+    
+    // TODO: Maybe make this reactive to save CPU
+    private void Update()
+    {
+        dashHud.SetActive(state.DashUnlocked);
+        laserHud.SetActive(state.LaserEyesUnlocked);
+    }
+}
