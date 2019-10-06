119c8d2e4d1cadc8e5b4071f65e23b94c8aedfb8
Mark Arneman <mark@arneman.me>
2019-10-06 19:39:27 -0400

    Added DoTween and unlock animations

---
+++ src/LDJam45/Assets/Scripts/AnimateOnUnlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimateOnUnlock : MonoBehaviour
+{
+    [SerializeField] private GameEvent UnlockEvent;
+    [SerializeField] private GameObject Icon;
+
+    private void OnEnable() {
+        UnlockEvent.Subscribe(() => {
+            Debug.Log("Unlock Event Triggered");
+            Vector3 EndPosition = Icon.transform.position;
+            Icon.transform.position = Icon.transform.position + new Vector3(150, 150, 0);
+            Icon.transform.localScale = new Vector3(2.0f, 2.0f);
+            Icon.transform.DOMove(EndPosition, 1.0f);
+            Icon.transform.DOScale(1, 1.0f);
+        }, this);
+    }
+
+    private void OnDisable() {
+        UnlockEvent.Unsubscribe(this);
+    }
+}
