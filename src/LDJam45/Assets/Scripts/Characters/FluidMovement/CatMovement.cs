7d77a3001d40648165b353ca54684871df85e36a
noah reinagel <noah.r9@gmail.com>
2019-10-06 11:54:15 -0700

    cleaned up cat movement

--- src/LDJam45/Assets/Scripts/Characters/FluidMovement/CatMovement.cs
+++ src/LDJam45/Assets/Scripts/Characters/FluidMovement/CatMovement.cs
@@ -6,20 +6,12 @@ public class CatMovement : MonoBehaviour
     public bool IsActive;
 
     [SerializeField] private float MoveSpeed = 5.0f;
+    [SerializeField] private CatIsOnGround CatIsOnGround;
+    [SerializeField] private Rigidbody CatBody;
+    [SerializeField] private Animator Animator;
 
-
-    private CatIsOnGround _catIsOnGround;
-    private Rigidbody _catBody;
-    private Animator _animator;
     private Vector3 _rotation;
     private Vector3 _inputs = Vector3.zero;
-
-    private void Start()
-    {
-        _catIsOnGround = GetComponent<CatIsOnGround>();
-        _catBody = GetComponent<Rigidbody>();
-        _animator = GetComponent<Animator>();
-    }
 
     private void Update()
     {
@@ -31,19 +23,19 @@ public class CatMovement : MonoBehaviour
         _inputs = Vector3.ClampMagnitude(Vector3.Normalize(new Vector3(_inputs.x, 0f, _inputs.z)), 1f);
         _rotation = Vector3.Normalize(new Vector3(_inputs.x, 0f, _inputs.z));
         if (_inputs != Vector3.zero)
-            transform.forward = _rotation;
+            CatBody.transform.forward = _rotation;
     }
 
     private void FixedUpdate()
     {
         if (!IsActive)
             return;
-        if (_inputs == Vector3.zero || !_catIsOnGround.IsOnGround)
-            _animator.SetBool("IsWalking", false);
+        if (_inputs == Vector3.zero || !CatIsOnGround.IsOnGround)
+            Animator.SetBool("IsWalking", false);
         else
         {
-            _animator.SetBool("IsWalking", true);
-            _catBody.AddForce(_inputs * MoveSpeed * Time.fixedDeltaTime, ForceMode.Force);
+            Animator.SetBool("IsWalking", true);
+            CatBody.AddForce(_inputs * MoveSpeed * Time.fixedDeltaTime, ForceMode.Force);
         }
     }
 }
