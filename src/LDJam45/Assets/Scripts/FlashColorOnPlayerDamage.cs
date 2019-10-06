fb795701cbfc1334e770c9b294434b34c24bc6f4
SilasReinagel <silas.reinagel@gmail.com>
2019-10-06 00:10:17 -0700

    Flashes red when player takes damage. Closes #116

---
+++ src/LDJam45/Assets/Scripts/FlashColorOnPlayerDamage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashColorOnPlayerDamage : MonoBehaviour
+{
+    [SerializeField] private GameEvent onDamaged;
+
+    private Image image;
+    private Color targetColor;
+    private Color targetTransparent;
+
+    bool wasFreshlyDamaged;
+
+    private void OnEnable()
+    {
+        image = GetComponent<Image>();
+        targetColor = new Color(image.color.r, image.color.g, image.color.b, 0.6f);
+        targetTransparent = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        onDamaged.Subscribe(() => wasFreshlyDamaged = true, this);
+    }
+
+    private void OnDisable()
+    {
+        onDamaged.Unsubscribe(this);
+    }
+
+    void Update()
+    {
+        if (wasFreshlyDamaged)
+            image.color = Color.Lerp(image.color, targetColor, 20 * Time.deltaTime);
+        else
+            image.color = Color.Lerp(image.color, targetTransparent, 20 * Time.deltaTime);
+
+        if (image.color.a >= 0.6)
+            wasFreshlyDamaged = false;
+    }
+}
