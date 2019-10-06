8490529fb80765659758db44657f9724dc52b97e
SilasReinagel <silas.reinagel@gmail.com>
2019-10-05 20:52:36 -0700

    Starts with No Abilities. Closes #100

--- src/LDJam45/Assets/State/GameState.cs
+++ src/LDJam45/Assets/State/GameState.cs
@@ -6,10 +6,18 @@ public class GameState : ScriptableObject
     public bool IsTravelling;
     public int CurrentHp;
     public int MaxHp = 3;
+    public bool DashUnlocked;
+    public bool SlashUnlocked;
+    public bool RendUnlocked;
+    public bool LaserEyesUnlocked;
 
     public void Reset()
     {
         CurrentHp = MaxHp;
         IsTravelling = false;
+        DashUnlocked = false;
+        SlashUnlocked = false;
+        RendUnlocked = false;
+        LaserEyesUnlocked = false;
     }
 }
