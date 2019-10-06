8ac74ab651db05a4eaa7163e43a84ea89644950b
Jaroslav Vozár <jarkovozar@gmail.com>
2019-10-06 14:55:07 +0200

    Added script for cat purring on mouse hover behaviour

---
+++ src/LDJam45/Assets/Scripts/Characters/CatPurring.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CatPurring : MonoBehaviour
+{
+    public AudioSource purrAudioSource;
+
+    private void OnMouseEnter()
+    {
+        purrAudioSource?.Play();
+        Debug.Log("Cat purrs.");
+    }
+
+    private void OnMouseExit()
+    {
+        purrAudioSource?.Stop();
+        Debug.Log("Cat stops purring.");
+    }
+}
