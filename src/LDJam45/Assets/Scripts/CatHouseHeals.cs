6262dd4e3c767210da642ed1349616a9931adda0
SilasReinagel <silas.reinagel@gmail.com>
2019-10-07 00:43:46 -0700

    Created Cat House. Closes #193

---
+++ src/LDJam45/Assets/Scripts/CatHouseHeals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CatHouseHeals : MonoBehaviour
+{
+    [SerializeField] private GameObject openDoor;
+    [SerializeField] private GameObject closedDoor;
+    [SerializeField] private float sleepDuration = 1.5f;
+
+    [SerializeField, ReadOnly] private bool isFinished;
+
+    private void Awake()
+    {
+        isFinished = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(HealSleep());
+    }
+
+    private IEnumerator HealSleep()
+    {
+        if (isFinished)
+            yield break;
+
+        closedDoor.SetActive(true);
+        openDoor.SetActive(false);
+
+        // TODO: Play Sleep Sound
+        yield return new WaitForSeconds(sleepDuration / 2);
+        // TODO: Heal One Health
+        yield return new WaitForSeconds(sleepDuration / 2);
+
+        // TODO: Close Door afterwards
+
+        openDoor.SetActive(true);
+        closedDoor.SetActive(false);
+        isFinished = true;
+    }
+}
