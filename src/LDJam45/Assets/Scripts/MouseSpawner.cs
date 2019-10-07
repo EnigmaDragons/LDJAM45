8379aa639cc59bc0777cdbde8254952132048187
noah reinagel <noah.r9@gmail.com>
2019-10-07 15:51:05 -0700

    made timer for mouse massacre

--- src/LDJam45/Assets/Scripts/MouseSpawner.cs
+++ src/LDJam45/Assets/Scripts/MouseSpawner.cs
@@ -2,19 +2,38 @@
 
 public class MouseSpawner : MonoBehaviour
 {
+    [SerializeField] private GameEvent StartEvent;
     [SerializeField] GameObject MousePrefab;
     [SerializeField] float SpawnTimeMin = 1.0f;
     [SerializeField] private float SpawnTimeMax = 2.0f;
+    [SerializeField] private float TotalTime = 60;
 
+    private bool _started;
     private float _timeTilNextSpawn;
+    private float _timeRemaining;
+
+    private void Start()
+    {
+        StartEvent.Subscribe(() =>
+        {
+            _timeRemaining = TotalTime;
+            _started = true;
+        }, this);  
+    }
 
     void Update()
     {
+        if (!_started)
+            return;
+
         _timeTilNextSpawn -= Time.deltaTime;
         if (_timeTilNextSpawn <= 0)
         {
             Instantiate(MousePrefab, transform.position, Quaternion.identity, transform);
             _timeTilNextSpawn = Random.Range(SpawnTimeMin, SpawnTimeMax);
         }
+        _timeRemaining -= Time.deltaTime;
+        if (_timeRemaining <= 0)
+            _started = false;
     }
 }
