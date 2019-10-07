4895de6ed4556f5c6b90d4cc08ce2cbdb71427a5
Jaroslav Vozár <jarkovozar@gmail.com>
2019-10-07 22:22:44 +0200

    Fixed UI in CreditsScene (no cursor showing and inability to select Back button with arrow keys/gamepad)

---
+++ src/LDJam45/Assets/Scripts/ActiveGameObjectSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveGameObjectSelector : MonoBehaviour
+{
+    public GameObject newActivePanel;
+
+    void Start()
+    {
+        ShowPanels showPanelsScript = FindObjectOfType<ShowPanels>();
+
+        if (showPanelsScript != null)
+        {
+            showPanelsScript.activePanel = newActivePanel;
+        }      
+    }
+}
