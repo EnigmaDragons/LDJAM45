8bc8ef3181c490c79c6e0b6e1544fef3f92991f6
SilasReinagel <silas.reinagel@gmail.com>
2019-10-07 15:25:47 -0700

    Can press Space to skip Cutscene camera. Closes #225

--- src/LDJam45/Assets/CoolCameraIntro.cs
+++ src/LDJam45/Assets/CoolCameraIntro.cs
@@ -67,14 +67,19 @@ public class CoolCameraIntro : MonoBehaviour
         _remainingDuration = durations[_index];
     }
 
+    private bool SkipRequested() => Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Escape);
+
     private void CheckSkipRequested()
     {
-        if (_showedSkipPrompt && Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape))
+        if (!SkipRequested())
+            return;
+
+        if (_showedSkipPrompt)
         {
             _shouldSkip = true;
             Finish();
         }
-        if (!_showedSkipPrompt && Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape))
+        if (!_showedSkipPrompt)
         {
             _showedSkipPrompt = true;
             state.ThoughtsMessageQueue.Enqueue("Press Enter again to skip.");
