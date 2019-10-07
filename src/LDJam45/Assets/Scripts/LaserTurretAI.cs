7369ac3927d33b884578a07e1817086f0cde8e3f
noah reinagel <noah.r9@gmail.com>
2019-10-07 10:09:37 -0700

    fixed attack serialization bug

--- src/LDJam45/Assets/Scripts/LaserTurretAI.cs
+++ src/LDJam45/Assets/Scripts/LaserTurretAI.cs
@@ -211,17 +211,3 @@ public enum LaserTurretAttackType
     Volley,
     Spin
 }
-
-[CreateAssetMenu]
-public class LaserTurretAttack : ScriptableObject
-{
-    public LaserTurretAttackState StartingState;
-    public LaserTurretAttackType Type;
-    public float Spread;
-    public float TimeBetweenShots;
-    public float AttackDuration;
-    public float RotationSpeed;
-    public float WindUpTime;
-    public float SeekingTime;
-    public float WindDownTime;
-}
--- src/LDJam45/Assets/Scripts/RoombaEnemy.cs
+++ src/LDJam45/Assets/Scripts/RoombaEnemy.cs
@@ -243,21 +243,3 @@ public enum RoombaAttackType
     Charge,
     Spin
 }
-
-[CreateAssetMenu]
-public class RoombaAttack : ScriptableObject
-{
-    public RoombaAttackState StartingState;
-    public RoombaAttackType Type;
-    public float AttackDuration;
-    public float WindUpTime;
-    public float ChasingTime;
-    public float WindDownTime;
-
-    public float ChaseSpeed;
-    public float ChaseAngularSpeed;
-    public float ChaseAcceleration;
-
-    public float Speed;
-    public float RotationSpeed;
-}
