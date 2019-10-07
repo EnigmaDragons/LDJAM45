e5e6ed13c4112115c5bc635236e70cdafc1a3eae
Mark Arneman <mark@arneman.me>
2019-10-07 15:26:38 -0400

    Thought Triggers for early levels

--- src/LDJam45/Assets/Scripts/LitterBox.cs
+++ src/LDJam45/Assets/Scripts/LitterBox.cs
@@ -6,25 +6,11 @@ public class LitterBox : MonoBehaviour
 {
     [SerializeField] GameObject DirtyLitterBox;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             if (!DirtyLitterBox.activeSelf) {
                 DirtyLitterBox.SetActive(true);
             }
-        }
-        
-        Debug.Log(other.gameObject.tag.ToString());
+        }               
     }
 }
