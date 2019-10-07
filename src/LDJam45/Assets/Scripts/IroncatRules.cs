3ffb6d1e7cd6548b90fa3fbf1791c22927432af8
noah reinagel <noah.r9@gmail.com>
2019-10-07 16:24:35 -0700

    added ironcat mode

---
+++ src/LDJam45/Assets/Scripts/IroncatRules.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class IroncatRules : MonoBehaviour
+{
+    [SerializeField] private GameState GameState;
+    [SerializeField] private GameEvent UnlockDash;
+    [SerializeField] private GameEvent UnlockSlash;
+
+    private bool _init;
+
+    private void Update()
+    {
+        if (_init)
+            return;
+        _init = true;
+        if (GameState.PlayIronmanMode)
+        {
+            UnlockDash.Publish();
+            UnlockSlash.Publish();
+        }
+    }
+}
