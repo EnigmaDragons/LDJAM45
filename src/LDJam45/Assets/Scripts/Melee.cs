b1436fb467beea9da9759e72881ee99e44785701
SilasReinagel <silas.reinagel@gmail.com>
2019-10-06 21:38:27 -0700

    Added Shock sound for Green Floor Hazard. Closes #184

--- src/LDJam45/Assets/Scripts/Melee.cs
+++ src/LDJam45/Assets/Scripts/Melee.cs
@@ -3,20 +3,17 @@
 public class Melee : MonoBehaviour
 {
     [SerializeField] private Role OwnedBy;
+    [SerializeField] private AudioClip OnHitSound;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other) => Trigger(other.gameObject.GetComponent<Health>());
+    private void OnTriggerEnter(Collider other) => Trigger(other.GetComponent<Health>());
+
+    private void Trigger(Health health)
     {
-        var health = other.gameObject.GetComponent<Health>();
         if (health == null || health.Role.Equals(OwnedBy))
             return;
         health.ApplyDamage();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        var health = other.GetComponent<Health>();
-        if (health == null || health.Role.Equals(OwnedBy))
-            return;
-        health.ApplyDamage();
+        if (OnHitSound != null)
+            AudioSource.PlayClipAtPoint(OnHitSound, Camera.current.transform.position);
     }
 }
