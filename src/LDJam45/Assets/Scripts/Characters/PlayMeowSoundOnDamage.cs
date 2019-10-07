023208deed8d67436aec2fcc542db915aeb14f5a
SilasReinagel <silas.reinagel@gmail.com>
2019-10-07 13:13:00 -0700

    Cat should play meow sounds on taking damage.

---
+++ src/LDJam45/Assets/Scripts/Characters/PlayMeowSoundOnDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+class PlayMeowSoundOnDamage : MonoBehaviour
+{
+    [SerializeField] private GameSceneSharedObjects shared;
+    [SerializeField] private GameEvent onDamaged;
+    [SerializeField] private AudioClip[] sounds;
+
+    void OnEnable()
+    {
+        onDamaged.Subscribe(PlaySound, this);
+    }
+
+    private void OnDisable()
+    {
+        onDamaged.Unsubscribe(this);
+    }
+
+    private void PlaySound()
+    {
+        Debug.Log("Ouch");
+        shared.catAudioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+    }
+}
