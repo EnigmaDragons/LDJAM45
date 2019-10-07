75fe23a5bf30ee29cf713b3ee41e5d0f49c91ac9
Mark Arneman <mark@arneman.me>
2019-10-06 21:48:53 -0400

    HP Presenter Updates and SuperDashRoom

--- src/LDJam45/Assets/Scripts/Characters/HpPresenter.cs
+++ src/LDJam45/Assets/Scripts/Characters/HpPresenter.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
 
 public class HpPresenter : MonoBehaviour
 {
@@ -6,31 +9,71 @@ public class HpPresenter : MonoBehaviour
     [SerializeField] private GameEvent onHealthGained;
     [SerializeField] private GameState state;
 
+    [SerializeField] private float Delay;
+
+    [SerializeField] private Sprite Health2;
+    [SerializeField] private Sprite Health3;
+
     private GameObject[] hpIcons;
+    private bool DelayTrigger = false;
 
     private void OnEnable()
     {
-        onHealthGained.Subscribe(UpdateHealth, this);
-        onHealthLost.Subscribe(UpdateHealth, this);
-        hpIcons = new GameObject[transform.childCount];
-        for (int i = 0; i < hpIcons.Length; ++i)
-            hpIcons[i] = transform.GetChild(i).gameObject;
+        onHealthGained.Subscribe(GainHealth, this);
+        onHealthLost.Subscribe(LoseHealth, this);                
     }
 
-    private void Start()
-    {
-        UpdateHealth();
-    }
-
-    private void OnDisable()
-    {
+    private void OnDisable() {
         onHealthLost.Unsubscribe(this);
         onHealthGained.Unsubscribe(this);
     }
 
-    void UpdateHealth()
+    private void Start()
     {
-        for (int i = 0; i < hpIcons.Length; ++i)
+        hpIcons = new GameObject[transform.childCount];
+        for (int i = 0; i < hpIcons.Length; ++i) {
+            hpIcons[i] = transform.GetChild(i).gameObject;
+        }
+
+        Debug.Log(hpIcons.Length);        
+    }
+
+    private void LateUpdate() {
+        // HACK: Delay until late update, GameState is 0 at Start
+        if (!DelayTrigger) {
+            DrawHealth();
+            DelayTrigger = true;
+        }
+    }
+
+    void DrawHealth()
+    {
+        Debug.Log(state.CurrentPlayerHp);
+        for (int i = 0; i < hpIcons.Length; ++i) {
+            //if (i > state.CurrentPlayerHp - 1) {
+            //    hpIcons[i].transform.localScale = Vector3.zero;
+            //}
             hpIcons[i].SetActive(i < state.CurrentPlayerHp);
+        }            
+    }
+
+    void GainHealth() {
+        hpIcons[state.CurrentPlayerHp].transform.DOScale(1, 1.0f).OnComplete(() => DrawHealth());
+    }
+
+    void LoseHealth() {
+        // hpIcons[state.CurrentPlayerHp].transform.DOScale(0, 1.0f).OnComplete(() => DrawHealth());
+        StartCoroutine(AnimateHealthLoss());        
+    }
+
+    IEnumerator AnimateHealthLoss() {
+        Image image = hpIcons[state.CurrentPlayerHp].GetComponent<Image>();        
+        image.sprite = Health2;
+        yield return new WaitForSeconds(Delay);
+
+        image.sprite = Health3;
+        yield return new WaitForSeconds(Delay);
+
+        hpIcons[state.CurrentPlayerHp].SetActive(false);
     }
 }
